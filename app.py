26e534c71abd50fdcf0c014d7b0e9108436ee1c4
koyalkarbhumika21 <koyalkarsukesh33@gmail.com>
2026-06-26 11:01:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -66,5 +66,9 @@ def predict():
     )
 
 # Run app
+import os
+
+# Run app
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
